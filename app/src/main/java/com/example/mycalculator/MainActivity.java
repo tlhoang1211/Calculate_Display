788700ce8b4e3298c1956837d80f0bb22d615216
Hoàng Trần <46571690+tlhoang1211@users.noreply.github.com>
2020-10-23 19:07:49 +0700

    initial commit

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
